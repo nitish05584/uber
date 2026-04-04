bfab12d63e69cb09a65e0fd1d693a9d695b5d325
nitish05584 <devnath8471@gmail.com>
2026-04-04 17:02:05 +0530

    auth middleware

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -17,10 +17,32 @@ const authUser=async(req,res,next)=>{
        next()
     } catch (error) {
          console.error("isAuth error");
-    return res.status(500).json({ message: "is auth error" });
+    return res.status(500).json({ message: " authUser is auth error" });
+    }
+}
+
+
+
+const authCaptain=async(req,res,next)=>{
+    try {
+        const {token}=req.cookies
+       if(!token){
+         return res.status(400).json({message:"user does not have a token"})
+       } 
+
+       const verifyToken=jwt.verify(token,process.env.JWT_SECRET)
+       if(!verifyToken){
+         return res.status(400).json({message:"user does not have a token"})
+       }
+       req.captId=verifyToken.captId
+       next()
+    } catch (error) {
+        console.error("isAuth error");
+     return res.status(500).json({ message: "is auth error" });
     }
 }
 
 module.exports={
-    isAuth
+    isAuth,
+    authCaptain,
 }
